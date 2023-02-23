70f9461afe5c5448049b6b13517d28ffdb8c5385
Nhoj <nhojplummer@yahoo.com>
2023-02-23 14:08:14 -0500

    Adding Font Awesome to html file for future us

--- front-end-project/src/components/Navbar/Navbar.js
+++ front-end-project/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          
+          <Link to="/" className="navbar-logo">LINK</Link>
         </div>
       </nav>
     </>
