fa32a9fefe62edb5d918e5b9743635dc7150ede1
Mario hernandez <69408092+maarioherns21@users.noreply.github.com>
2023-03-24 09:30:57 +0800

    Update product page

--- front-end-project/src/components/pages/Products.js
+++ front-end-project/src/components/pages/Products.js
@@ -2,7 +2,33 @@ import React from "react"
 import "../../App.css"
 
 function Products() {
-  return <h1 className="products">PRODUCTS</h1>
+  const [products. setProducts]=useState([])
+  
+  const fetchData = async () => {
+   try {
+      const res = await fetch(//the end point for your productions/aka backend)
+      const data =  await res.json()
+      
+      setProducts(data)
+   }catch(err) {
+   console.log(err)
+   }
+  }
+  
+  
+  //// here you can  map  the data that you fetch from the backend//
+  return (
+   <div>
+    <h1 className="products"></h1>
+    
+    
+//     {products.map((product) => (
+//     <div key={product._id}>
+//      <h2>{product.name}</h2>
+//     </div>
+//   ))}
+    </div>
+  )
 }
 
 export default Products
